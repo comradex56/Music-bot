da1dafc7efe494e69a76bfd7922e394f64768096
Comrade xploit <79577718+comradex56@users.noreply.github.com>
2024-03-07 18:09:58 +0530

    Config.py

--- Config.py
+++ Config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "1954786730"))
-    API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
+    API_HASH = os.environ.get("API_HASH", "cf7a3453abd8bc841a495ba655d2cef0")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
